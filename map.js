ba9d14fc61c6533b1381bbecc31eef10e6255cb4
Isak La Fleur <isak.engdahl@gmail.com>
2017-02-19 11:12:55 +0100

    .

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 ///////////////////////
 // ES6 - forEach helper
 ///////////////////////
-
+/*
 var numbers = [1,2,3];
 
 var doubleNumbers = [];
@@ -20,3 +20,56 @@ var doubled = numbers.map(function(number) {
 });
 
 console.log(doubled);
+// =====================================
+
+
+var cars = [
+    { model: 'Buick', price: 'CHEAP' },
+    { model: 'Camaro', price: 'expensive' }
+];
+
+// ES6 - .map
+var prices = cars.map(function(car) {
+    return car.price;
+});
+
+console.log(prices);
+
+*/
+// =====================================
+
+// Plucking values
+
+var images = [
+  { height: '34px', width: '39px' },
+  { height: '54px', width: '19px' },
+  { height: '83px', width: '75px' },
+];
+
+var heights = images.map(function(image){
+    return image.height;
+});
+
+// Calculating Values with Map
+
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+var speeds = trips.map(function(trip) {
+    return trip.distance / trip.time;
+
+});
+console.log(speeds);
+
+// Really Hard - Implementing 'Pluck'
+
+var paints = [ { color: 'red' }, { color: 'blue' }, { color: 'yellow' }];
+
+function pluck(paints, color) {
+    return paints.map(function(paint) {
+        return paint['color'];
+    });
+}
