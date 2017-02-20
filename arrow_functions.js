e0ca04563a15879b54ad712643fe80c92b303977
Isak La Fleur <isak.engdahl@se.atlascopco.com>
2017-02-20 11:03:23 +0100

    .

---
+++ arrow_functions.js
@@ -0,0 +1,15 @@
+//////////////////////////
+// ES6 - Arrow Functions
+//////////////////////////
+
+// ES5
+const add1 = function(a, b) {
+    return a + b;
+}
+console.log(add1(1, 2));
+
+// ES6
+const add2 = (a, b) => {
+    return a + b;
+}
+console.log(add2(1, 2));
--- template_strings.js
+++ template_strings.js
@@ -40,3 +40,29 @@ console.log(data);
 // ES6 with template String
 const data2 = `{ "device_id": "${device_id}", "guid": "${guid}", "username": "${username}","}`;
 console.log(data2);
+
+// ==============================================
+
+// Template Strings in Practice
+
+// ES5
+function doubleMessage(number) {
+  return "Your number doubled is " + (2 * number);
+}
+
+// ES6 
+function doubleMessage(number) {
+  return `Your number doubled is ${2 * number}`;
+}
+
+// =================================================
+
+// ES5
+function fullName(firstName, lastName) {
+  return firstName + lastName;
+}
+
+// ES6
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
